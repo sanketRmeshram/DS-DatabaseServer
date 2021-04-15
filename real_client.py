2896ce7efb274dcd00bdcfceb90787d6b88397a7
sanketRmeshram <48240880+sanketRmeshram@users.noreply.github.com>
2021-04-15 16:13:39 +0530

    added real client

---
+++ real_client.py
@@ -0,0 +1,31 @@
+import socket
+import struct
+import json
+
+print("enter public ip of host : ")
+HOST = input().replace(" ", "").replace('\n', "")
+PORT = 1104
+
+
+
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT)) 
+    msg = input()
+    msg = {
+        "isRead" : False,
+        "sanket": msg
+    }
+    msg = json.dumps(msg)
+    msg = msg.encode('utf-8')
+    msg_sz = struct.pack(">H", len(msg))
+    s.sendall(msg_sz+msg)
+    print("sended")
+    sz = s.recv(2)
+    sz = struct.unpack(">H", sz)[0]
+    # print(sz)
+    response = s.recv(sz)
+    response = response.decode('utf-8')
+    response = json.loads(response)
+    print("received : ",response)
+
