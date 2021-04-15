a15a769e3b6c1a3eceece3535a518f646585884d
sanketRmeshram <48240880+sanketRmeshram@users.noreply.github.com>
2021-04-15 21:45:45 +0530

    made requests in real client in round robin fashion

--- real_client.py
+++ real_client.py
@@ -1,19 +1,24 @@
 import socket
 import struct
 import json
+import urllib.request
 
-print("enter public ip of host : ")
-HOST = input().replace(" ", "").replace('\n', "")
+# print("enter public ip of host : ")
+# HOST = input().replace(" ", "").replace('\n', "")
 PORT = 1104
-
+url = "https://gist.githubusercontent.com/sanketRmeshram/2e0c71add59402cc26f1a518e425e0a8/raw/all_ip.txt"
+HOST = [_.decode("utf-8").replace(" ", "").replace('\n', "") for _ in urllib.request.urlopen(url)]
+total_node = len(HOST)
+index = 0
+print(HOST)
 
 
 while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT)) 
+    s.connect((HOST[index], PORT)) 
     msg = input()
     msg = {
-        "isRead" : False,
+        "isRead" : True,
         "sanket": msg
     }
     msg = json.dumps(msg)
@@ -29,5 +34,7 @@ while True:
     response = response.decode('utf-8')
     response = json.loads(response)
     print("received : ",response)
+
+    index = (index+1)%total_node
 
 
